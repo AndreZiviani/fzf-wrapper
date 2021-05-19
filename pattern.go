1a4292bb13ce83129c9c0269f45077f98b42d464
Andre Ziviani <andrepziviani@gmail.com>
2021-05-19 12:21:26 -0300

    feat/sort: add option to sort results by score, length and/or position

--- pattern.go
+++ pattern.go
@@ -5,11 +5,21 @@ import (
 	"github.com/junegunn/fzf/src/algo"
 )
 
-func MatchChunk(chunk *Chunk, pattern [][]rune) []Result {
+type Pattern struct {
+	chunk   *Chunk
+	pattern [][]rune
+	sortBy  []criterion
+}
+
+func NewPattern(chunk *Chunk, pattern [][]rune, sortBy []criterion) *Pattern {
+	return &Pattern{chunk, pattern, sortBy}
+}
+
+func (p Pattern) MatchChunk() []Result {
 	matches := []Result{}
-	items := chunk.items
-	for idx := 0; idx < chunk.count; idx++ {
-		match, offsets, pos := MatchItem(&items[idx], pattern)
+	items := p.chunk.items
+	for idx := 0; idx < p.chunk.count; idx++ {
+		match, offsets, pos := p.MatchItem(&items[idx])
 		if match != nil {
 			match.Offsets = offsets
 			match.Pos = pos
@@ -20,17 +30,17 @@ func MatchChunk(chunk *Chunk, pattern [][]rune) []Result {
 	return matches
 }
 
-func MatchItem(item *Item, pattern [][]rune) (*Result, []fzf.Offset, *[]int) {
-	offsets, bonus, pos := extendedMatch(item, pattern)
-	if len(offsets) == len(pattern) {
-		result := buildResult(item, offsets, bonus)
+func (p Pattern) MatchItem(item *Item) (*Result, []fzf.Offset, *[]int) {
+	offsets, bonus, pos := p.extendedMatch(item)
+	if len(offsets) == len(p.pattern) {
+		result := buildResult(item, offsets, bonus, p.sortBy)
 		return &result, offsets, pos
 	}
 
 	return nil, nil, nil
 }
 
-func extendedMatch(item *Item, pattern [][]rune) ([]fzf.Offset, int, *[]int) {
+func (p Pattern) extendedMatch(item *Item) ([]fzf.Offset, int, *[]int) {
 
 	input := []Token{{&item.text, 0}}
 
@@ -38,12 +48,12 @@ func extendedMatch(item *Item, pattern [][]rune) ([]fzf.Offset, int, *[]int) {
 	allPos := &[]int{}
 	var totalScore int
 
-	for _, term := range pattern {
+	for _, term := range p.pattern {
 		matched := false
 		var offset fzf.Offset
 		var currentScore int
 
-		off, score, pos := iter(input, term)
+		off, score, pos := p.iter(input, term)
 
 		if sidx := off[0]; sidx >= 0 {
 			offset, currentScore = off, score
@@ -67,7 +77,7 @@ func extendedMatch(item *Item, pattern [][]rune) ([]fzf.Offset, int, *[]int) {
 	return offsets, totalScore, allPos
 }
 
-func iter(tokens []Token, pattern []rune) (fzf.Offset, int, *[]int) {
+func (p Pattern) iter(tokens []Token, pattern []rune) (fzf.Offset, int, *[]int) {
 	for _, part := range tokens {
 		if res, pos := algo.FuzzyMatchV2(false, true, true, part.text, pattern, true, nil); res.Start >= 0 {
 			sidx := int32(res.Start) + part.prefixLength
